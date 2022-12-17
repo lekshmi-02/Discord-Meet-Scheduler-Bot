8a58dc6087d5392733201b44331ff60f58ddb9bc
devi-priya22 <sdevipriya803@gmail.com>
2022-12-18 04:54:17 +0530

    added a function

--- venv/main.py
+++ venv/main.py
@@ -29,13 +29,7 @@ async def on_message(msg):
         if msg.content=="hello":
             await msg.send("hai")
 
-def convert_time(hr,min):
-    hr=int(input[0:2])
-    min=int(input[2:])
-    postfix="PM"
-    if hr<12:
-        postfix="AM"
-    return '{}:{:02d}{}'.format(hr or 12,min,postfix)
+
 
 
 
