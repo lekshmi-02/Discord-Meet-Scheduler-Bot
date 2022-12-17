b782508812707cc554e033f371880da7ecf2c4b9
Vyshnavi-V <vvyshnavi2020@gmail.com>
2022-12-17 12:09:22 +0530

    updated main.py

--- venv/main.py
+++ venv/main.py
@@ -1,2 +1,23 @@
-print("Hiiiiiii")
-print("hello")
+import discord
+import os
+import asyncio
+import random
+from dotenv import load_dotenv
+from discord.ext import commands
+
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix = '!',intents =discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print("Bot is connected to discord")
+
+
+
+
+
+bot.run(token)
